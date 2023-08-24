2087480893f0f71d59ef2171dfba430b337449bc
Stokes <astokes1@una.edu>
2023-08-24 12:56:42 -0500

    Added a jew things to the app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send<h1>('Hello World From Express')
+  res.send('<h1>Hello World From Express</h1>')
 })
 
 app.listen(3000)
